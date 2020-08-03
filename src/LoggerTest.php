524da766274bddcd249cdd6b3e539ae8617a104e
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-08-03 13:37:57 +0300

    fix method test format

--- src/LoggerTest.php
+++ src/LoggerTest.php
@@ -24,6 +24,22 @@ class LoggerTest extends TestCase
             $logType = $val;
             $logMessage = 'log';
             $this->assertEquals("[{$logType}] {$logMessage}" . PHP_EOL, $this->logger->format($logType, $logMessage, [], false));
+
+            $placeholderLogMessage = "{$logMessage} {test} {scalar_int} {scalar_float} {scalar_string} {scalar_boolean} {scalar_boolean_false}";
+            $logMessage .= '  1 1.01 123 1 ';
+            $handle = fopen(__DIR__ . DIRECTORY_SEPARATOR . 'StdObject.php', 'r');
+            $context = [
+                'test' => null,
+                'scalar_int' => 1,
+                'scalar_float' => 1.01,
+                'scalar_string' => '123',
+                'scalar_boolean' => true,
+                'scalar_boolean_false' => false,
+//                'object' => new \StdObject(),
+//                'date_time' => (new \DateTime())->format(\DateTime::RFC3339),
+//                'resource'=> $handle
+            ];
+            $this->assertEquals("[{$logType}] {$logMessage}" . PHP_EOL, $this->logger->format($logType, $placeholderLogMessage, $context, false));
         }, array_keys(AbstractLogger::LEVELS));
     }
 }
---
+++ src/StdObject.php
@@ -0,0 +1,9 @@
+<?php
+
+class StdObject
+{
+    public function __toString()
+    {
+        return '123';
+    }
+}
